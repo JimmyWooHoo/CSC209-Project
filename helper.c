ead6ffaefff7c9e100693f44059ba3adc9d75303
JimmyWooHoo <songxuanwu6@gmail.com>
2026-03-27 16:25:46 -0400

    Added parent proecess logic

--- helper.c
+++ helper.c
@@ -1,166 +1,144 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include "node.h"
 
-struct Node {
-	char word[80];
-	int count;
-	struct Node *next;
-}
-
-
-char **read_words(char *filename) {
-	
-	File *fp;
-	char **words;
-
-	fp = fopen(filename, "r");
-	if (!fp) {
+char **read_words(const char *filename) {
+	FILE *fp = fopen(filename, "r");
+	if (fp == NULL) {
 		perror("fopen");
 		exit(1);
 	}
-	
+
 	int count = 0;
 	char str[81];
-	// count how many words in total in this file
 	while (fscanf(fp, "%80s", str) == 1) {
 		count++;
 	}
 	rewind(fp);
 
-	words = malloc((sizeof(*words) * count) + 1);
+	char **words = malloc(sizeof(*words) * (count + 1));
 	if (words == NULL) {
 		perror("malloc");
+		fclose(fp);
 		exit(1);
 	}
-	
+
 	int i = 0;
 	while (fscanf(fp, "%80s", str) == 1) {
-		if(strcspn(str, "\n")) {
-			str[strcspn(str, "\n")] = '\0';
-		} else { // ensure buffer is null-terminated
-			str[80] = '\0';
-		}
 		words[i] = malloc(strlen(str) + 1);
- 		strcpy(words[i], str);
+		if (words[i] == NULL) {
+			perror("malloc");
+			fclose(fp);
+			deallocate_words(words);
+			exit(1);
+		}
+		strcpy(words[i], str);
 		i++;
 	}
-	
+
 	words[count] = NULL;
 	fclose(fp);
 	return words;
 }
 
 void deallocate_words(char **words) {
-	char **p = words;
-	while (*p) {
+	if (words == NULL) {
+		return;
+	}
+
+	for (char **p = words; *p != NULL; p++) {
 		free(*p);
-		p++;
 	}
 	free(words);
 }
 
-
-Node *new_node(char *str) {
+Node *new_node(const char *str) {
 	Node *new = malloc(sizeof(Node));
 	if (new == NULL) {
 		return NULL;
 	}
-	
-	new->word == malloc(strlen(str) + 1);
-	if (!new->word) {
-		free(new);
-		return NULL;
-	}
 
-	strcpy(new->word, str);
+	strncpy(new->word, str, sizeof(new->word) - 1);
+	new->word[sizeof(new->word) - 1] = '\0';
 	new->count = 1;
 	new->next = NULL;
-
 	return new;
 }
 
-void add_word_to_node(Node *n, char *str) {
-	if(strcmp(n->word, str) == 0) {
-		n->count++;
-	}
-}
-
-Node *find_node(Family *node_list, char *str) {
-	Node *head = node_list;
-	while(head != NULL) {
-		if(strcmp(head->word, str) == 0) {
-			// early exit if the word exists
-			return head;
+Node *find_node(Node *node_list, const char *str) {
+	Node *curr = node_list;
+	while (curr != NULL) {
+		if (strcmp(curr->word, str) == 0) {
+			return curr;
 		}
-		head = head->next;
+		curr = curr->next;
 	}
-	// no such word exists;
 	return NULL;
 }
 
 Node *generate_node_family(char **word_list) {
 	Node *head = NULL;
-	char **word = word_list;
-	
+	Node *tail = NULL;
 
-	while (*word != NULL) {
-		Node *found = find_node(head, word);
-		if (!found) {
-			Node *n = new_node(word);
-			if(!head) {
-				head = n;
-				tail = n;
-			} else {
-				tail->next = n;
-				tail = tail->next;
-			}
-	
-		} else {
-			add_word_to_node(found, word);
+	for (char **word = word_list; *word != NULL; word++) {
+		Node *found = find_node(head, *word);
+		if (found != NULL) {
+			found->count++;
+			continue;
 		}
-		word++;
-	}
-	
-	return head;
 
+		Node *new = new_node(*word);
+		if (new == NULL) {
+			perror("malloc");
+			deallocate_nodes(head);
+			return NULL;
+		}
+
+		if (head == NULL) {
+			head = new;
+			tail = new;
+		} else {
+			tail->next = new;
+			tail = new;
+		}
+	}
+
+	return head;
 }
 
-char *convert_node_family(Node *n) {
-	int count = 0;
-	int numwords = 0;
-	Node *curr = n;
-	while (curr != NULL) {
-		count = count + strlen(curr->word);
-		numwords++;
-		curr = curr->next;
+void add_count_to_node(Node *node_list, const char *str, int count) {
+	Node *found = find_node(node_list, str);
+	if (found != NULL) {
+		found->count += count;
 	}
-	
-	char *str = malloc(count + numwords);
-	strcpy(n->word, str);
-	numwords--;
-	n = n->next;
-	while(n != NULL) {
-		strncat(str, n->word, sizeof(str) - strlen(str) - 1);
-		numwords--;
-		if (numwords > 0) {
-			strcat(str, " ");
-		}
-		n = n->next;
+}
+
+char *convert_node_family(Node *node_list) {
+	int total_len = 1;
+	for (Node *curr = node_list; curr != NULL; curr = curr->next) {
+		total_len += snprintf(NULL, 0, "%s %d\n", curr->word, curr->count);
 	}
-	
-	str[strlen(str) + 1] = '\0';
+
+	char *str = malloc(total_len);
+	if (str == NULL) {
+		return NULL;
+	}
+
+	str[0] = '\0';
+	for (Node *curr = node_list; curr != NULL; curr = curr->next) {
+		char line[128];
+		snprintf(line, sizeof(line), "%s %d\n", curr->word, curr->count);
+		strcat(str, line);
+	}
+
 	return str;
 }
 
 void deallocate_nodes(Node *node_list) {
-	while (node_list) {
+	while (node_list != NULL) {
 		Node *next = node_list->next;
-		free(node_list->word);
 		free(node_list);
-		node_list = next;	
+		node_list = next;
 	}
 }
-
-
--- node.h
+++ node.h
@@ -1,20 +1,20 @@
 #ifndef NODE_H
 #define NODE_H
 
-struct node {
+typedef struct node {
 	char word[80];
 	int count;
-	struct Node *next;
-};
-typedef struct node Node;
+	struct node *next;
+} Node;
 
-char **read_words(char *filename);
+char **read_words(const char *filename);
 void deallocate_words(char **words);
-Node *new_node(char *str);
-void add_word_to_node(Node *n, char *str);
-Node *find_node(Family *node_list, char *str);
+
+Node *new_node(const char *str);
+Node *find_node(Node *node_list, const char *str);
 Node *generate_node_family(char **word_list);
-char *convert_node_family(Node *n);
+void add_count_to_node(Node *node_list, const char *str, int count);
+char *convert_node_family(Node *node_list);
 void deallocate_nodes(Node *node_list);
 
 #endif
